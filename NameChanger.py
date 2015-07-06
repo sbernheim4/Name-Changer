e690526f8136c21855ed307f0333007a1bc210f0
sbernheim4 <sambernheim@gmail.com>
2015-07-06 00:10:20 -0400

    added comments

--- NameChanger.py
+++ NameChanger.py
@@ -2,12 +2,15 @@ __author__ = 'samuelbernheim'
 
 from os import listdir, rename, system
 
-path = "/Users/samuelbernheim/Desktop/FilesToRename/"
-sourceDir = listdir(path)
+# path of the directory which houses the files whose names will be changed
+sourcePath = "/Users/samuelbernheim/Desktop/FilesToRename/"
+sourceDir = listdir(sourcePath)
 
+# copies the files from the source directory into the project directory
 for theFile in sourceDir:
     system("cp /Users/samuelbernheim/Desktop/FilesToRename/* /Users/samuelbernheim/Python/NameChanger/")
 
+# renames the files 
 for theFile in sourceDir:
 
     newName = "Image " + theFile[25:28] + ".jpg"
