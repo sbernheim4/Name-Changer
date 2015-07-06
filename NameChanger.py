5de62715c432879ebcd7ac80caba47b4b85e074c
sbernheim4 <sambernheim@gmail.com>
2015-07-06 00:02:02 -0400

    program now copies from source loaction automatically

--- NameChanger.py
+++ NameChanger.py
@@ -1,9 +1,12 @@
 __author__ = 'samuelbernheim'
 
-from os import listdir, rename
+from os import listdir, rename, system
 
 path = "/Users/samuelbernheim/Desktop/FilesToRename/"
 sourceDir = listdir(path)
+
+for theFile in sourceDir:
+    system("cp /Users/samuelbernheim/Desktop/FilesToRename/* /Users/samuelbernheim/Python/NameChanger/")
 
 for theFile in sourceDir:
 
