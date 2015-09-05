e0844e6acb7e3f2e3d7a3c3488ca0546a4899313
sbernheim4 <sambernheim@gmail.com>
2015-09-04 23:17:00 -0400

    improved getTheFullTitile method

--- NameChanger.py
+++ NameChanger.py
@@ -33,21 +33,35 @@ def removePeriodsFromName(name):
             duplicate = duplicate + letter
     return duplicate
 
+
+def removeUnderscoresFromName(name):
+    currentPosition = 0
+    duplicate = ""
+    for letter in name:
+        currentPosition = currentPosition + 1
+        if letter == "_":
+            duplicate = duplicate + " "
+        else:
+            duplicate = duplicate + letter
+    return duplicate
+
 # Use this function if the current names of your files contain specific attributes like individual names
 # Test this before hand in a separate python file to make sure the output is correct
-def getTheFullTitle(rawNameOfEpisode):
-
-    series = raw_input("Enter the Series variable")
-
+def getTheFullTitle(rawNameOfEpisode, nameOfShow):
 
     # TODO: Modify this variable to make sure the numbers are correct for your specific batch of files.
-    episodeInfo = rawNameOfEpisode[15:21]
+    episodeInfo = rawNameOfEpisode[21:31]
 
-    nameSize = len(rawNameOfEpisode)
+    episodeInfoLength = len(episodeInfo)
+    episodeInfo = "S01E" + episodeInfo[-2:episodeInfoLength]
+
+    print "Episode Info: " + episodeInfo
 
     # TODO: This line must be modified for every new set that will use this function
     # gets the part of the title from after the episode info (S01E01) to the end of the name
-    usableName = rawNameOfEpisode[22:nameSize]
+    nameSize = len(rawNameOfEpisode)
+    usableName = rawNameOfEpisode[34:nameSize]
+    print "Usable Name: " + usableName
 
     # counter variable
     currentPosition = 0
@@ -56,20 +70,19 @@ def getTheFullTitle(rawNameOfEpisode):
     # parses through the usableName to find the first toccurence of 1 which would be in 1080p and gets that position
     for letter in usableName:
         currentPosition = currentPosition + 1
-        if letter == "1":
+        if letter == "[":
             break
 
     # variable for the title of the episode
     titleOfEpisode = usableName[0:currentPosition-2]
+    titleOfEpisode = removeUnderscoresFromName(titleOfEpisode)
     print "Episode Title: " + titleOfEpisode
 
-    # In case periods are used in place of spaces, this will change all periods to spaces
-    titleOfEpisode = removePeriodsFromName(titleOfEpisode)
-
-    # this way either variable can be used and the same result will be output
-    fullName = series + " - " + episodeInfo + " - " + titleOfEpisode + ".mkv"
+    # this way either variable can be used and the same 3result will be output
+    fullName = nameOfShow + " - " + episodeInfo + " - " + titleOfEpisode + ".mkv"
 
     return fullName
+
 
 # path of the directory which houses the files whose names will be changed
 sourcePath = "/Users/samuelbernheim/Desktop/FilesToRename/"
@@ -85,22 +98,25 @@ system("cp /Users/samuelbernheim/Desktop/FilesToRename/* /Users/samuelbernheim/P
 
 fileNumber = 1
 
+nameOfShow = raw_input("Enter the name of the show\n")
+
 # renames the files
 for eachFile in sourceDir:
-
 
     # if the file name does not contain any information you wish to preserve use the following if block. Otherwise use
     # the getTheFullTitle function. Just be sure to run that using an example name before running this program on your
     # batch of files.
 
     if eachFile != ".DS_Store":
-        # if using fileNumber, use this if statement to have the same number of digits ex: going from 09 to 10
-        if fileNumber < 10:
-            newName = "Attack on Titan - S01E0" + str(fileNumber) + ".mp4"
-        else:
-            newName = "Attack on Titan - S01E" + str(fileNumber) + ".mp4"
+        # # if using fileNumber, use this if statement to have the same number of digits ex: going from 09 to 10
+        # if fileNumber < 10:
+        #     newName = "Attack on Titan - S01E0" + str(fileNumber) + ".mp4"
+        # else:
+        #     newName = "Attack on Titan - S01E" + str(fileNumber) + ".mp4"
+        #
+        # print "New Name: " + newName
 
-        print "New Name: " + newName
+        newName = getTheFullTitle(eachFile, nameOfShow)
 
         print newName
 
@@ -116,10 +132,9 @@ system("rm /Users/samuelbernheim/Desktop/FilesToRename/*")
 # TODO: FEW LETTERS OF WHAT EVERY FILE WILL BEGIN WITH.
 
 # copies all the files which begin with "Image" in the project folder, into the source folder with the right name
-system("cp /Users/samuelbernheim/Python/Name-Changer/The* /Users/samuelbernheim/Desktop/FilesToRename/")
+system("cp /Users/samuelbernheim/Python/Name-Changer/Cow* /Users/samuelbernheim/Desktop/FilesToRename/")
 
 # removes the files from the project folder
-system("rm The*")
-
+system("rm Cow*")
 
 print "Program completed"
